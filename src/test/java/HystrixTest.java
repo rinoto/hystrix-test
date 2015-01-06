b810616ce371611e52dc8855182c1c09596b8372
Ruben Inoto <rinoto@real.com>
2015-01-06 12:38:33 +0100

    a

--- src/test/java/HystrixTest.java
+++ src/test/java/HystrixTest.java
@@ -1,8 +1,12 @@
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.Test;
 
 public class HystrixTest {
 	@Test
-	public void testName() throws Exception {
-
+	public void dummyTest() throws Exception {
+		String s = new CommandHelloWorld("Bob").execute();
+		assertThat(s, is("Hello Bob!"));
 	}
 }
